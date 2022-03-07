bc507ad070aa61c6ee58cfd021dd113d8f54cb85
solcloud <74121353+solcloud@users.noreply.github.com>
2022-03-07 13:18:14 +0100

    provider: use defaults setPrefetch()  sizeOctets 0 size

--- src/SolcloudProvider.php
+++ src/SolcloudProvider.php
@@ -173,7 +173,7 @@ class SolcloudProvider implements ServiceProviderInterface
             $worker->setFailedExchange(HashMap::get('consumer.failedExchange', null, false));
             $worker->setFailedRoutingKey(HashMap::get('consumer.failedRoutingKey', null, false));
             $worker->setMaximumNumberOfProcessedMessages(HashMap::get('consumer.maximumNumberOfProcessedMessages'));
-            $worker->setPrefetch(HashMap::get('consumer.prefetch.count', 1), HashMap::get('consumer.prefetch.sizeOctets', 0));
+            $worker->setPrefetch(HashMap::get('consumer.prefetch.count', 1));
 
             return $worker;
         };
