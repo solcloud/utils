ab131be39d15c0face457670a289f63c2f3837d8
solcloud <74121353+solcloud@users.noreply.github.com>
2022-06-02 15:35:01 +0200

    Update workerRunV1.php

--- worker/workerRunV1.php
+++ worker/workerRunV1.php
@@ -7,11 +7,11 @@ use Solcloud\Utils\Logger;
 
 /* @var $worker \Solcloud\Consumer\BaseConsumer */
 $worker = $container['worker'];
-$worker->consume(HashMap::get('consumer.consumeQueue'), HashMap::get('consumer.noAck'));
+$worker->consume(HashMap::get('consumer.consumeQueue'), HashMap::get('consumer.noAck', false));
 
 while ($worker->hasCallback()) {
     try {
-        $worker->wait(mt_rand(HashMap::get('consumer.channel.wait.minSec'), HashMap::get('consumer.channel.wait.maxSec')));
+        $worker->wait(mt_rand(HashMap::get('consumer.channel.wait.minSec', 3600), HashMap::get('consumer.channel.wait.maxSec', 4800)));
     } catch (Exception $ex) {
         if ($ex instanceof NumberOfProcessedMessagesExceed || $ex instanceof AMQPTimeoutException) {
             Logger::exception($ex, Logger::TRACE);
